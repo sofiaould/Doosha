5befabd44679d5964fbbdea041ae987251abdae8
Ting-Chi Wong <tcwong007@gmail.com>
2021-06-01 21:39:01 +0200

    best version 01/06/21 a 21h38

--- routes/article.routes.js
+++ routes/article.routes.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const router  = express.Router();
 const Article = require("../models/article.model");
+const fileUploader = require("../configs/cloudinary.config");
 
 /////////////////////////// article home ////////////////////////////////
 router.get('/homearticles', (req, res, next) => {
@@ -10,11 +11,11 @@ router.get('/homearticles', (req, res, next) => {
     .catch(err => next(err))
   ;
 });
-router.post('/homearticles', (req, res, next) => {
+router.post('/homearticles', fileUploader.single("image"), (req, res, next) => {
   const title = req.body.title;
   const text = req.body.text;
   const date = req.body.date;
-  const image = req.body.image;
+  const image = req.file.path;
   const article = new Article({
     title,
     text,
@@ -23,7 +24,7 @@ router.post('/homearticles', (req, res, next) => {
   });
   article.save()
     .then(article => {
-      res.redirect('/articles');
+      res.redirect('/homearticles');
     })
     .catch(err => {
       res.render('articles/articleform');
@@ -36,13 +37,29 @@ router.get('/formarticle', (req, res, next) => {
   res.render('articles/articleform')
 });
 
+// // .get() route ==> to display the article form to users
+// router.get("/formarticle", (req, res) => res.render("articles/articleform"));
+
+// // .post() route ==> to process form data
+// router.post("/formarticle", fileUploader.single("image"), (req, res, next) => {
+//   const { title, text, date } = req.body;
+//   const image = req.file.path;
+//   if (!title || !text || !date) {
+//     res.render("articles/articleform", {
+//       errorMessage:
+//         "All fields are mandatory. Please provide your title, your text, date.",
+//     });
+//     return;
+//   }
+// });
+
 ////////////////////// article selected ////////////////////////////
-router.get('/:id', (req, res, next) => {
-  Article.findOne({_id: req.params.id})
-    .then(article => res.render('articles/articleselected', {article}))
-    .catch(err => next(err))
-  ;
-});
+// router.get('/:id', (req, res, next) => {
+//   Article.findOne({_id: req.params.id})
+//     .then(article => res.render('articles/articleselected', {article}))
+//     .catch(err => next(err))
+//   ;
+// });
 
 
 module.exports = router;
